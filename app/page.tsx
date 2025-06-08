48c428b5fd7609f3d3d3dc785ba970fac2e959a4
Furkan <frknyrdm0@gmail.com>
2025-06-08 19:18:14 +0300

    v1.0

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import SkipHire from "./components/SkipHire";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-50 py-8">
-      <SkipHire/>
+    <main className="min-h-screen bg-indigo-200 py-8">
+      <SkipHire />
     </main>
   );
 }
